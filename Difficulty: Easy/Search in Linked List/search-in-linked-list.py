659f8a8210f717eb75bd89915106df3b3001dc6a
Swaksy7781 <saurabhwakaseseb159@gmail.com>
2025-01-04 12:58:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.py
@@ -0,0 +1,53 @@
+#User function Template for python3
+
+
+''' Node of a linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def searchKey(self, n, head, key):
+        temp = head
+        
+        for i in range(n):
+            if temp.data == key:
+                return 1
+            else:
+                temp = temp.next
+        return 0
+        #Code here
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        data = list(map(int, input().strip().split()))
+        head = Node(data[0])
+        tail = head
+        for i in range(1, n):
+            tail.next = Node(data[i])
+            tail = tail.next
+        key = int(input())
+        ob = Solution()
+        res = ob.searchKey(n, head, key)
+        key = 0
+        if res == True:
+            print("true")
+        else:
+            print("false")
+
+        print("~")
+
+# } Driver Code Ends
